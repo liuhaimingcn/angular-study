148d79b44d2ae7bcc02d6b6e0a810463e11bf52d
liuhaiming <liuhaiming.cn@gmail.com>
2015-03-08 16:20:03 +0800

    添加

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # angular-study
 angular学习中编写的代码
+
+学习地址
+    http://www.w3cschool.cc/angularjs/angularjs-directives.html
---
+++ controller/nameController.js
@@ -0,0 +1,9 @@
+function nameController ($scope) {
+    $scope.names = [
+        {name:"zzhangsan1"},
+        {name:"Thangsan2"},
+        {name:"Qhangsan3"},
+        {name:"Ihangsan4"},
+        {name:"shangsan5"}
+    ]
+}
